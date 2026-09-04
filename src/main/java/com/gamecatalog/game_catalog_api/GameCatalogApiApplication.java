81cf65c7e2b8ac3345f8b4b6fbdebc2147ccf07c
Joao-V07 <Joaovitorodasilva@hotmail.com>
2026-09-04 19:13:10 +1000

    Project's first commit: Spring boot initializr setup.

---
+++ src/main/java/com/gamecatalog/game_catalog_api/GameCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamecatalog.game_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamecatalog/game_catalog_api/GameCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamecatalog.game_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
